2110b65cd8b662009b03f8b14a4079682909ddc4
myungum <gmyungum@gmail.com>
2022-11-01 01:07:54 +0900

    수집서버 변경에 따른 DB 포팅

--- make_statistics.py
+++ make_statistics.py
@@ -9,22 +9,23 @@ import os
 from soynlp.word import WordExtractor
 from soynlp.tokenizer import MaxScoreTokenizer
 import math
+import json
+import traceback
+import sys
 
 UPDATE_PERIOD = 600
 MIN_COUNT = 24
 TOKENIZER_DIR = os.getcwd() + '/soynlp_tokenizer'
 TOKENIZER_TRAIN_RANGE = 7
 TOKENIZER_TRAIN_CHAT_SIZE = 10 ** 7
+AVAILABLE_DATE_RANGE = 14
 
 trash_list = open('불용어.txt', 'r', encoding='utf8').read().splitlines()
 without_hangul = re.compile('[^ ㄱ-ㅎㅏ-ㅣ가-힣+]')
 
 # connection info
-with open('setting.txt', 'r') as f:
-    host, port, db_name = f.readline().split()
-    chat_server_ip, chat_server_port = f.readline().split()
-    oauth_token = f.readline()
-    client_id, client_secret = f.readline().split()
+with open('settings.json', 'r') as file:
+    conn_info = dict(json.load(file))
 client = None
 db = None
 
@@ -32,46 +33,92 @@ db = None
 def add_log(msg):
     now = datetime.now()
     print('[{}] {}'.format(now, msg))
-    db['log'].insert_one({
-        'source': __file__,
-        'msg': msg,
-        'datetime': now
-    })
+    # db['log'].insert_one({
+    #     'source': __file__,
+    #     'msg': msg,
+    #     'datetime': now
+    # })
 
+def get_dates(base: datetime, to_str: bool):
+    date_list = [base - timedelta(days=x+1) for x in range(AVAILABLE_DATE_RANGE)]
+    if to_str:
+        date_list = [date.strftime('%Y-%m-%d') for date in date_list]
+    return date_list
 
-def get_missing_dates(today, collection_name):
+def get_missing_dates(today: datetime, collection_name: str, to_str: bool):
     missing_dates = list(
-        set(db['chats'].distinct('date'))
+        set(get_dates(today, to_str=True))
         - set(db[collection_name].distinct('date')))
     # convert str into date
-    missing_dates = [datetime.strptime(
-        date_str, '%Y-%m-%d').date() for date_str in missing_dates]
-    # remove not finished date
-    missing_dates = [date for date in missing_dates if date < today]
+    if not to_str:
+        missing_dates = [datetime.strptime(
+            date_str, '%Y-%m-%d') for date_str in missing_dates]
     # sort
     missing_dates.sort(reverse=True)
     return missing_dates
 
 
-def get_chats(target_date, max_chat_count):
-    week = [(target_date
-                 - timedelta(days=i)).strftime('%Y-%m-%d') for i in range(TOKENIZER_TRAIN_RANGE)]
+def get_chats(target_date: datetime):
+    lower_bound = target_date
+    upper_bound = lower_bound + timedelta(days=1)
+    error = 0
+    docs = list(db['chat'].find({'$and': [{
+                    'datetime' : {'$gte' : lower_bound}
+                },
+                    {
+                    'datetime' : {'$lt' : upper_bound}
+                }, {
+                    'message' : {'$regex' : 'PRIVMSG'}
+                }]}).sort('datetime', 1))
+    # remove duplicated chat
+    msg_dic = dict()
+    front = 0
+    rear = 1
+    msg_dic[docs[0]['message']] = 1
+    removed = set()
+
+    while front < len(docs):
+        # push
+        while rear < len(docs) and (docs[rear]['datetime'] - docs[front]['datetime']).total_seconds() < 10:
+            if docs[rear]['message'] in msg_dic:
+                if msg_dic[docs[rear]['message']] > 0:
+                    error += 1
+                    removed.add(docs[rear]['_id'])
+                msg_dic[docs[rear]['message']] += 1
+            else:
+                msg_dic[docs[rear]['message']] = 1
+            rear += 1
+        
+        if rear >= len(docs):
+            break
+
+        if docs[rear]['message'] in msg_dic:
+            msg_dic[docs[rear]['message']] += 1
+        else:
+            msg_dic[docs[rear]['message']] = 1
+        rear += 1
+        
+        # pop
+        while front < rear and (docs[rear - 1]['datetime'] - docs[front]['datetime']).total_seconds() >= 10:
+            msg_dic[docs[front]['message']] -= 1
+            front += 1
+
     chats = []
-
-    for date_str in tqdm(week):
-        docs = list(db['chats'].find({'date': date_str}))
-
-        for doc in docs:
-            chat = without_hangul.sub('', doc['text']).strip()
-            if len(chat) > 0:
-                chats.append(chat)
-                if len(chats) >= max_chat_count:
-                    return chats
+    for doc in docs:
+        if doc['_id'] not in removed:
+            try:
+                msg = doc['message']
+                chat = msg.split(':', 2)[-1]
+                chat = chat.strip()
+                if len(chat) > 0:
+                    chats.append(chat)
+            except:
+                pass
     return chats
 
 
-def make_word_frequency(today):
-    missing_dates = get_missing_dates(today, 'word_frequency')
+def make_word_frequency(today: datetime):
+    missing_dates = get_missing_dates(today, 'word_frequency', to_str=False)
 
     if len(missing_dates) > 0:
         for date in tqdm(missing_dates):
@@ -79,10 +126,10 @@ def make_word_frequency(today):
             tokenizer = get_tokenizer(date)
             add_log('make word frequency: {}'.format(date_str))
             counter = Counter()
-            for doc in tqdm(list(db['chats'].find({'date': date_str}, {'_id': 0, 'text': 1}))):
+            for chat in get_chats(date):
                 word_set = set()
-                chat = without_hangul.sub('', doc['text']).strip()
-                for word in tokenizer.tokenize(chat):
+                hangul = without_hangul.sub('', chat).strip()
+                for word in tokenizer.tokenize(hangul):
                     word_set.add(word)
                 counter.update(word_set)
 
@@ -98,8 +145,8 @@ def make_word_frequency(today):
             })
 
 
-def make_word_rank(today):
-    missing_dates = get_missing_dates(today, 'word_rank')[:-TOKENIZER_TRAIN_RANGE]
+def make_word_rank(today: datetime):
+    missing_dates = get_missing_dates(today, 'word_rank', to_str=False)
 
     if len(missing_dates) > 0:
         for date in tqdm(missing_dates):
@@ -136,7 +183,7 @@ def make_word_rank(today):
             })
 
 
-def get_tokenizer(target_date):
+def get_tokenizer(target_date: datetime):
     if not os.path.exists(TOKENIZER_DIR):
         os.mkdir(TOKENIZER_DIR)
 
@@ -147,7 +194,7 @@ def get_tokenizer(target_date):
         add_log('make tokenizer : {}'.format(tokenizer_file_name))
 
         # make tokenizer
-        chats = get_chats(target_date, TOKENIZER_TRAIN_CHAT_SIZE)
+        chats = get_chats(target_date)
         word_extractor = WordExtractor()
         word_extractor.train(chats)
         word_score_table = word_extractor.extract()
@@ -171,25 +218,20 @@ def get_tokenizer(target_date):
         return pickle.load(f)
 
 
-while True:
-    try:
-        # db connection
-        client = MongoClient(host=host, port=int(port))
-        db = client[db_name]
-        start_time = datetime.now()
+try:
+    # db connection
+    client = MongoClient(host=conn_info['db_host'], port=conn_info['db_port'])
+    db = client[conn_info['db_name']]
+    start_time = datetime.now()
+    today = start_time.replace(hour=0, minute=0, second=0, microsecond=0)
 
-        try:
-            make_word_frequency(start_time.date())
-            make_word_rank(start_time.date())
-        except Exception as e:
-            add_log('Exception: {}'.format(str(e)))
+    make_word_frequency(today)
+    make_word_rank(today)
 
-        elapsed_time = datetime.now() - start_time
-        add_log('elapsed time: {}'.format(str(elapsed_time)))
-        if UPDATE_PERIOD > elapsed_time.seconds:
-            time.sleep(UPDATE_PERIOD - elapsed_time.seconds)
-
-    except Exception as e:
-        add_log('Exception: {}'.format(str(e)))
-    finally:
-        client.close()
+    elapsed_time = (datetime.now() - start_time).total_seconds()
+    add_log('elapsed time: {}'.format(str(elapsed_time)))
+    time.sleep(max(UPDATE_PERIOD - elapsed_time, 0))
+except:
+    traceback.print_exc(file=sys.stderr)
+finally:
+    client.close()
